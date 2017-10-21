5bf2b34644a2febd0c39052f99277c203b082fd6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-10-21 21:47:37 +0700

    Add program dir

---
+++ Notes/progs/01_simple.f90
@@ -0,0 +1,11 @@
+PROGRAM simple01
+  IMPLICIT NONE
+  INCLUDE 'mpif.h'
+  INTEGER :: rank, Nprocs, ierr
+  CALL MPI_Init(ierr)
+  CALL MPI_Comm_size(MPI_COMM_WORLD, Nprocs, ierr)
+  CALL MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+  WRITE(*,*) 'Hello, I am proc: ', rank, ' from ', Nprocs, ' total processes'
+  CALL MPI_Finalize(ierr)
+END PROGRAM
+
