d19349f0b21a93f66d7c6b2d175d60fd4c83fa9a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-12-21 00:43:02 +0700

    Add notes about compilers that can be used

--- Notes/codes/f90/01_simple.f90
+++ Notes/codes/f90/01_simple.f90
@@ -1,11 +1,11 @@
 PROGRAM simple
   IMPLICIT NONE
   INCLUDE 'mpif.h'
-  INTEGER :: rank, Nprocs, ierr
+  INTEGER :: my_rank, Nprocs, ierr
   CALL MPI_Init(ierr)
   CALL MPI_Comm_size(MPI_COMM_WORLD, Nprocs, ierr)
-  CALL MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
-  WRITE(*,*) 'Hello, I am proc: ', rank, ' from ', Nprocs, ' total processes'
+  CALL MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr)
+  WRITE(*,*) 'Hello, I am proc: ', my_rank, ' from ', Nprocs, ' total processes'
   CALL MPI_Finalize(ierr)
 END PROGRAM
 
