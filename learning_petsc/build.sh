4e52af2ac2de5c9cef870162bc8f3f13e5b3208a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-09-10 21:02:34 +0700

    Fix build.sh

--- learning_petsc/build.sh
+++ learning_petsc/build.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
 PETSC_DIR1=/home/efefer/WORKS/PETSC/petsc-3.11.3/
 PETSC_DIR2=/home/efefer/WORKS/PETSC/petsc-3.11.3/arch-linux2-c-debug
 
-mpicc.mpich $1 -I${PETSC_DIR1}/include -I${PETSC_DIR2}/include -L${PETSC_DIR2}/lib -lpetsc
+if [ "$#" -eq 1 ]; then
 
-# -lblas -llapack -lm -lX11 -ldl
+  basnam=`basename $1 .c`
+  mpicc.mpich $1 -I${PETSC_DIR1}/include -I${PETSC_DIR2}/include -L${PETSC_DIR2}/lib -lpetsc -o $basnam.x
+  # -lblas -llapack -lm -lX11 -ldl
+
+else
+
+  echo "Wrong number of arguments: $#"
+
+fi
+
--- learning_petsc/calc_e.c
+++ learning_petsc/calc_e.c
@@ -4,28 +4,25 @@ int main(int argc, char **argv) {
   
   PetscErrorCode ierr;
   
-  int            rank, i;
-  double         localval, globalsum;
+  int rank, i;
+  double localval, globalsum;
 
-  PetscInitialize(&argc,&argv,NULL, "Compute e in parallel with PETSc.\n\n");
+  PetscInitialize(&argc, &argv, NULL, "Compute e in parallel with PETSc.\n\n");
   
-  ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank); CHKERRQ(ierr);
+  ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank); CHKERRQ(ierr);
   
   // compute  1/n!  where n = (rank of process) + 1
   localval = 1.0;
-  for (i = 2; i < rank+1; i++)
-      localval /= i;
+  for (i = 2; i < rank+1; i++) {
+    localval /= i;
+  }
   
   // sum the contributions over all processes
-  ierr = MPI_Allreduce(&localval, &globalsum, 1, MPI_DOUBLE, MPI_SUM,
-      PETSC_COMM_WORLD); CHKERRQ(ierr);
+  ierr = MPI_Allreduce(&localval, &globalsum, 1, MPI_DOUBLE, MPI_SUM, PETSC_COMM_WORLD); CHKERRQ(ierr);
   
   // output one estimate of e and report on work from each process
-  ierr = PetscPrintf(PETSC_COMM_WORLD,
-      "e is about %17.15f\n", globalsum); CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_SELF,
-      "rank %d did %d flops\n", rank, (rank > 0) ? rank-1 : 0);
-      CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD, "e is about %17.15f\n", globalsum); CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_SELF, "rank %d did %d flops\n", rank, (rank > 0) ? rank-1 : 0); CHKERRQ(ierr);
   
   return PetscFinalize();
 }
